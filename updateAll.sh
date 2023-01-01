eddd9033a25c916c5e17cddc59ee59ba0569401d
Stefan Geißelsöder <stefangeisselsoeder-github@yahoo.de>
2023-01-02 00:15:38 +0100

    cleaner update output, no timeout anymore

--- updateAll.sh
+++ updateAll.sh
@@ -7,8 +7,8 @@ for i in `ls | grep -v -e "$(basename $0)" -e "\.log" -e "\.git" -e "\.txt" -e "
 do
 cd $i
 echo "### Updating next: " $i
-# git pull | grep -v -e "Already up-to-date" -e "Bereits aktuell" -e "Already up to date"
-timeout $MY_TIMEOUT_IN_S git pull
+git pull | grep -v -e "Already up-to-date" -e "Bereits aktuell" -e "Already up to date"
+# timeout $MY_TIMEOUT_IN_S git pull
 cd $BASEPATH
 done
 
