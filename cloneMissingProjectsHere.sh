84409f1c86165f22da815959b3151cdf94dfe02c
Stefan Geißelsöder <stefan@geisselsoeder.com>
2023-01-03 20:54:40 +0100

    incomplete attempt at gitgoup cloning

--- cloneMissingProjectsHere.sh
+++ cloneMissingProjectsHere.sh
@@ -55,26 +55,57 @@ do
 done
 
 # 2022_ki1 @ geis.ddnss.de
+GROUPNAME="2022_ki1_gitgroup"
+mkdir -p $GROUPNAME
 for projectName in 2022_ki1_vorlesung 2022_ki1_a3_loesung 2022_ki1_a3 2022_ki1_uebung_bewerten 2022_ki1_a1 2022_ki1_a2_loesung 2022_ki1_a2 2022_ki1_a2.5 2022_ki1_a4_loesung 2022_ki1_a5_loesung 2022_ki1_a6_loesung 2022_ki1_a7_loesung 2022_ki1_a4 2022_ki1_a5 2022_ki1_a3 2022_ki1_probeklausur 2022_ki1_klausur
 do
-	if [ ! -d "$projectName" ]; then
-		git clone git@geis.ddnss.de:2022_ki1_material/${projectName}.git ${projectName}
+	if [ ! -d "${GROUPNAME}/${projectName}" ]; then
+		git clone git@geis.ddnss.de:2022_ki1_material/${projectName}.git ${GROUPNAME}/${projectName}
 	fi
 done
 
 # 2022_ki3 @ geis.ddnss.de
+GROUPNAME="2022_ki3_gitgroup"
+mkdir -p $GROUPNAME
 for projectName in 2022_ki3_vorlesung 2022_ki3_a1_loesung 2022_ki3_a1 2022_ki3_a2_loesung 2022_ki3_a2 2022_ki3_a3_loesung 2022_ki3_klausur 2022_ki3_a4 2022_ki3_a4_loesung 2022_ki3_a5_loesung 2022_ki3_a5 2022_ki3_klausuraufgaben
 do
-	if [ ! -d "$projectName" ]; then
-		git clone git@geis.ddnss.de:2022_ki3_material/${projectName}.git ${projectName}
+	if [ ! -d "${GROUPNAME}/${projectName}" ]; then
+		git clone git@geis.ddnss.de:2022_ki3_material/${projectName}.git ${GROUPNAME}/${projectName}
 	fi
 done
 
-# website_static
-for projectName in wise21_p1_a5_loesung wise21_p1_a6_loesung wise21_p1_a7_loesung wise21_p1_a8_loesung wise21_ki1_a4_loesung wise21_ki1_a5_loesung wise21_ki1_a6_loesung wise21_ki1_a7_loesung wise21_p1_a5 wise21_p1_a6 wise21_p1_a7 wise21_p1_a8 wise21_ki1_a4 wise21_ki1_a5 wise21_ki1_a6 wise21_ki1_a7 wise21_p1_klausur wise21_ki1_klausur wise21_ki1_probeklausur wise21_p1_probeklausur wise21_ki1_klausuraufgaben wise21_ki1_klausurloesung wise21_p1_klausuraufgaben wise21_p1_klausurloesung wise21_p1_klausur 2022_sose_ki2_vorlesung 2022_sose_biolern_vorlesung 2022_sose_ki2_a1_loesung 2022_sose_ki2_a2_loesung demo_nn 2022_sose_p1_klausur 2022_sose_ki1_klausur 2022_sose_p1_klausuraufgaben 2022_sose_p1_klausur_vorbenotung 
+
+for projectName in 2022_sose_biolern_vorlesung demo_nn  # website_static
 do
-	if [ ! -d "$projectName" ]; then
+	if [ ! -d "${projectName}" ]; then
 		git clone git@git-kik.hs-ansbach.de:sgeisselsoeder/${projectName}.git ${projectName}
+	fi
+done
+
+GROUPNAME="2021_p1_gitgroup"
+mkdir -p $GROUPNAME
+for projectName in wise21_p1_a5_loesung wise21_p1_a6_loesung wise21_p1_a7_loesung wise21_p1_a8_loesung wise21_p1_a5 wise21_p1_a6 wise21_p1_a7 wise21_p1_a8 wise21_p1_klausur wise21_p1_probeklausur wise21_p1_klausuraufgaben wise21_p1_klausurloesung wise21_p1_klausur 2022_sose_p1_klausur 2022_sose_p1_klausuraufgaben 2022_sose_p1_klausur_vorbenotung
+do
+	if [ ! -d "${GROUPNAME}/${projectName}" ]; then
+		git clone git@git-kik.hs-ansbach.de:sgeisselsoeder/${projectName}.git ${GROUPNAME}/${projectName}
+	fi
+done
+
+GROUPNAME="2021_ki1_gitgroup"
+mkdir -p $GROUPNAME
+for projectName in wise21_ki1_a4_loesung wise21_ki1_a5_loesung wise21_ki1_a6_loesung wise21_ki1_a7_loesung wise21_ki1_a4 wise21_ki1_a5 wise21_ki1_a6 wise21_ki1_a7 wise21_ki1_klausur wise21_ki1_probeklausur wise21_ki1_klausuraufgaben wise21_ki1_klausurloesung 2022_sose_ki2_vorlesung 2022_sose_ki2_a1_loesung 2022_sose_ki2_a2_loesung 2022_sose_p1_klausur 2022_sose_ki1_klausur
+do
+	if [ ! -d "${GROUPNAME}/${projectName}" ]; then
+		git clone git@git-kik.hs-ansbach.de:sgeisselsoeder/${projectName}.git ${GROUPNAME}/${projectName}
+	fi
+done
+
+GROUPNAME="2022_ki2_gitgroup"
+mkdir -p $GROUPNAME
+for projectName in 2022_sose_ki2_vorlesung 2022_sose_ki2_a1_loesung 2022_sose_ki2_a2_loesung 2022_sose_p1_klausur 2022_sose_ki1_klausur
+do
+	if [ ! -d "${GROUPNAME}/${projectName}" ]; then
+		git clone git@git-kik.hs-ansbach.de:sgeisselsoeder/${projectName}.git ${GROUPNAME}/${projectName}
 	fi
 done
 
