59ae429bf8358e1fd58681b60d6da468bb5247b8
Stefan Geißelsöder <stefan@geisselsoeder.com>
2023-03-05 22:41:26 +0100

    fixes inconsistent projectnames

--- cloneMissingProjectsHere.sh
+++ cloneMissingProjectsHere.sh
@@ -95,7 +95,7 @@ done
 
 GROUPNAME="2022_ki2"
 mkdir -p ${GROUPNAME}${GITGROUPTAG}
-for projectName in 2022_sose_ki2_vorlesung \
+for projectName in 2022_ki2_vorlesung \
 	2022_sose_ki2_a1 2022_sose_ki2_a1_loesung \
 	2022_sose_ki2_a2 2022_sose_ki2_a2_loesung \
 	2022_sose_ki2_a3 2022_sose_ki2_a3_loesung \
