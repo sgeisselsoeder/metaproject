db82a866f181515d39538e30343ada826065ca74
Stefan Geißelsöder <stefan@geisselsoeder.com>
2023-04-06 09:42:32 +0200

    adds more student projects

--- cloneMissingProjectsHere.sh
+++ cloneMissingProjectsHere.sh
@@ -20,7 +20,7 @@ done
 GROUPNAME="2023_studentenprojekte"
 mkdir -p ${GROUPNAME}${GITGROUPTAG}
 for projectName in allgemeine_info reinforcementlearningescape produktinhalt autoencoder kognitivekooperation robo \
-	eeg-dl eeg-explainable
+	eeg-dl eeg-explainable sprachassistent osu_beatmap_discriminator deepfake pepperGPT
 do
 	if [ ! -d "${GROUPNAME}${GITGROUPTAG}/${projectName}" ]; then
 		git clone git@${GITLAB_HS}:${GROUPNAME}/${projectName}.git ${GROUPNAME}${GITGROUPTAG}/${projectName}
