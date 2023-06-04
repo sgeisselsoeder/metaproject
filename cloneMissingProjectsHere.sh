0880b381f857656abdcc374d7ac42339574fe543
Stefan Geißelsöder <stefan.geisselsoeder@hs-ansbach.de>
2023-06-04 11:28:03 +0200

    adds new RL project

--- cloneMissingProjectsHere.sh
+++ cloneMissingProjectsHere.sh
@@ -60,8 +60,8 @@ for projectName in 2023_ki4_vorlesung \
 	2023_ki4_a5 2023_ki4_a5_loesung \
 	2023_ki4_a6 2023_ki4_a6_loesung_1 2023_ki4_a6_loesung_2 \
 	2023_ki4_a7 2023_ki4_a7_loesung 2023_ki4_weitsprung \
-	2023_ki4_a8_loesung rl_playground
-
+	2023_ki4_a8 2023_ki4_a8_loesung rl_playground \
+	2023_ki4_a9_loesung
 do
 	if [ ! -d "${GROUPNAME}${GITGROUPTAG}/${projectName}" ]; then
 		git clone git@${GITLAB_HS}:${GROUPNAME}_material/${projectName}.git ${GROUPNAME}${GITGROUPTAG}/${projectName}
