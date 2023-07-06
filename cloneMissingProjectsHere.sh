13c68bff5e5fb2760ab8043225203e0611b4c5d4
Stefan Geißelsöder <stefan@geisselsoeder.com>
2023-07-06 09:17:31 +0200

    two more presentation projects

--- cloneMissingProjectsHere.sh
+++ cloneMissingProjectsHere.sh
@@ -228,7 +228,9 @@ done
 for projectName in blender3d pacman \
 	kooperation berufung arbeitszeugnisse buch_sigurd_carsten checklist_industrial_ai \
 	2023_01_16_stammtisch_ankit_cvision studentenprojektausschreibungen \
-	network_output_unequals_probability 2023_06_01_rothenburg_cvision_and_robots 2023_06_14_neuendettelsau_chancen_risiken # ownnn gitlab_korrektur
+	network_output_unequals_probability 2023_06_01_rothenburg_cvision_and_robots 2023_06_14_neuendettelsau_chancen_risiken \
+	2023_07_bmt_fleximaus 2023_07_07_schacht 
+	# ownnn gitlab_korrektur
 do
 	if [ ! -d "$projectName" ]; then
 		git clone git@${EIGEN_GITLAB}:sgeisselsoeder/${projectName}.git ${projectName}
