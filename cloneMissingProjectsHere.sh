f427bda8b049e4c94a49b294be9bd00fde055808
Stefan Geißelsöder <stefan.geisselsoeder@hs-ansbach.de>
2023-12-05 11:50:43 +0100

    adds more uebungs projects

--- cloneMissingProjectsHere.sh
+++ cloneMissingProjectsHere.sh
@@ -33,7 +33,9 @@ for projectName in 2023_ki3_vorlesung \
 	2023_ki3_a1 2023_ki3_a1_loesung \
 	2023_ki3_a2 2023_ki3_a2_loesung \
 	2023_ki3_a3 2023_ki3_a3_loesung \
-	2023_ki3_a4 2023_ki3_a4_loesung 
+	2023_ki3_a4 2023_ki3_a4_loesung \ 
+	2023_ki3_a5 2023_ki3_a5_loesung \
+	2023_ki3_a6 2023_ki3_a6_loesung 
 do
 	if [ ! -d "${GROUPNAME}${GITGROUPTAG}/${projectName}" ]; then
 		git clone git@${GITLAB_HS}:${GROUPNAME}_material/${projectName}.git ${GROUPNAME}${GITGROUPTAG}/${projectName}
