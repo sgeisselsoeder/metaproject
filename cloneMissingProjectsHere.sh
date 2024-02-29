2b9dc36db834be068e971738aa129e1a06a4df49
Stefan Geißelsöder <stefan@geisselsoeder.com>
2024-02-29 23:09:29 +0100

    adds second flaskauth tutorial project

--- cloneMissingProjectsHere.sh
+++ cloneMissingProjectsHere.sh
@@ -37,7 +37,7 @@ done
 
 GROUPNAME="webapp"
 mkdir -p ${GROUPNAME}${GITGROUPTAG}
-for projectName in flask_auth 
+for projectName in flask_auth flask_auth2 
 do
 	if [ ! -d "${GROUPNAME}${GITGROUPTAG}/${projectName}" ]; then
 		git clone git@${GITLAB_HS}:${GROUPNAME}/${projectName}.git ${GROUPNAME}${GITGROUPTAG}/${projectName}
