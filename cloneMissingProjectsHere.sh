a5508bf8680832a3fc4ef90a5ba99a88df15d0ce
Stefan Geißelsöder <stefan@geisselsoeder.com>
2024-03-10 22:05:12 +0100

    updates some tags

--- cloneMissingProjectsHere.sh
+++ cloneMissingProjectsHere.sh
@@ -17,7 +17,7 @@ for projectName in 2024_ki4_vorlesung \
 	2024_ki4_a4 2024_ki4_a4_loesung \
 	2024_ki4_a5 2024_ki4_a5_loesung \
 	2024_ki4_a6 2024_ki4_a6_loesung \
-	2024_ki4_a7 2024_ki4_a7_loesung 2023_ki4_weitsprung \
+	2024_ki4_a7 2024_ki4_a7_loesung 2024_ki4_weitsprung \
 	2024_ki4_a8 2024_ki4_a8_loesung \
 	2024_ki4_a9 2024_ki4_a9_loesung \
 	2024_ki4_a10 2024_ki4_a10_loesung
@@ -29,10 +29,10 @@ done
 
 GROUPNAME="2024_ki2"
 mkdir -p ${GROUPNAME}${GITGROUPTAG}
-for projectName in 2023_ki2_vorlesung \
-	2023_ki2_a1 2023_ki2_a1_loesung \
-	2023_ki2_a2 2023_ki2_a2_loesung \
-	2023_ki2_a3 2023_ki2_a3_loesung 
+for projectName in 2024_ki2_vorlesung \
+	2024_ki2_a1 2024_ki2_a1_loesung \
+	2024_ki2_a2 2024_ki2_a2_loesung \
+	2024_ki2_a3 2024_ki2_a3_loesung 
 do
 	if [ ! -d "${GROUPNAME}${GITGROUPTAG}/${projectName}" ]; then
 		git clone git@${GITLAB_HS}:${GROUPNAME}_material/${projectName}.git ${GROUPNAME}${GITGROUPTAG}/${projectName}
