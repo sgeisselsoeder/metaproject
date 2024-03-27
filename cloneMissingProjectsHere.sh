b822c8b8478368881ee0b1b15179786a1fcd10fd
Stefan Geißelsöder <stefan.geisselsoeder@hs-ansbach.de>
2024-03-27 12:02:59 +0100

    adds more student projects

--- cloneMissingProjectsHere.sh
+++ cloneMissingProjectsHere.sh
@@ -39,7 +39,7 @@ done
 
 GROUPNAME="2024_studentenprojekte"
 mkdir -p ${GROUPNAME}${GITGROUPTAG}
-for projectName in hautkrebs 
+for projectName in hautkrebs drohne eegelektroden elektrodenmessung serviceroboter 3Danomaly shootair frontend 
 do
 	if [ ! -d "${GROUPNAME}${GITGROUPTAG}/${projectName}" ]; then
 		git clone git@${GITLAB_HS}:${GROUPNAME}/${projectName}.git ${GROUPNAME}${GITGROUPTAG}/${projectName}
