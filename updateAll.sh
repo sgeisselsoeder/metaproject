68f5515a7c48a10dcc86956f09cdce6daaac383f
Stefan Geißelsöder <stefan@geisselsoeder.com>
2024-04-10 19:29:28 +0200

    debug output

--- updateAll.sh
+++ updateAll.sh
@@ -31,10 +31,11 @@ cd $BASEPATH
 done
 
 
-echo "Updating the cloneMissing, updateAll and statusAll scripts in the main projects folder with the content of metaproject."
-# cp metaproject/*.sh .
+echo "Updating the cloneMissing, updateAll and statusAll scripts in the main projects folder with the content of metaproject"
 cp metaproject/updateAll.sh .
+echo "1"
 cp metaproject/statusAll.sh .
+echo "2"
 cp metaproject/cloneMissingProjectsHere.sh .
 echo "Done copying new scripts to project folder"
 
