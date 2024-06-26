bbf9d1f6d603fdabf3fafa4c633909727bff07c2
Stefan Geißelsöder <stefan@geisselsoeder.com>
2024-06-26 22:17:45 +0200

    more ki2 projects

--- cloneMissingProjectsHere.sh
+++ cloneMissingProjectsHere.sh
@@ -35,8 +35,11 @@ for projectName in 2024_ki2_vorlesung \
 	2024_ki2_a1 2024_ki2_a1_loesung \
 	2024_ki2_a2 2024_ki2_a2_loesung \
 	2024_ki2_a3 2024_ki2_a3_loesung 
-	# 2024_ki2_a4 2024_ki2_a4_loesung 
-	# 2024_ki2_a5 2024_ki2_a5_loesung 
+	2024_ki2_a4_loesung \
+	2024_ki2_a5_loesung \
+	2024_ki2_a6_loesung \
+	2024_ki2_a7 2024_ki2_a7_loesung \
+	2024_ki2_a8 2024_ki2_a8_loesung
 do
 	if [ ! -d "${GROUPNAME}${GITGROUPTAG}/${projectName}" ]; then
 		git clone git@${GITLAB_HS}:${GROUPNAME}_material/${projectName}.git ${GROUPNAME}${GITGROUPTAG}/${projectName}
