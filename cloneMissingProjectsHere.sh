0a5f42b7adcdfe3bf8f41dd17980276b6b82c015
Stefan Geißelsöder <stefan@geisselsoeder.com>
2025-09-29 10:36:29 +0200

    removes duplicate

--- cloneMissingProjectsHere.sh
+++ cloneMissingProjectsHere.sh
@@ -519,7 +519,7 @@ done
 
 GROUPNAME="pawprint"
 mkdir -p ${GROUPNAME}${GITGROUPTAG}
-for projectName in 2024_studiprojekt pawpoc pawai pawdata backend
+for projectName in 2024_studiprojekt pawai pawdata backend
 do
 	if [ ! -d "${GROUPNAME}${GITGROUPTAG}/${projectName}" ]; then
 		git clone git@${GITLAB_HS}:${GROUPNAME}/${projectName}.git ${GROUPNAME}${GITGROUPTAG}/${projectName}
